e17d618b467371bd52ecf55f168777aa02423fae
CYMR-1 <cymr1234@outlook.com>
2025-12-23 23:03:06 +0800

    状态数据操作支持B指令

--- src/PoseHandler.hpp
+++ src/PoseHandler.hpp
@@ -13,6 +13,8 @@ public:
     void Move(void) noexcept;
     void TurnLeft(void) noexcept;
     void TurnRight(void) noexcept;
+    void Reverse(void) noexcept;
+    bool IsReverse(void) const noexcept;
     void Fast(void) noexcept;
     bool IsFast(void) const noexcept;
     Pose Query(void) const noexcept;
@@ -20,5 +22,6 @@ public:
 private:
     Pose pose;
     bool fast{false};
+    bool reverse{false};
 };
 }  // namespace adas
