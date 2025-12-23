0213a20887c8007cac37d80dc74e4d74f7583c1c
CYMR-1 <cymr1234@outlook.com>
2025-12-23 19:28:07 +0800

    extract Move()

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -11,63 +11,41 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : pose(pose)
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
     for (const auto cmd : commands) {
-        if (cmd == 'M')
-        {
-            if (pose.heading == 'E')
-            {
-                ++pose.x;
-            }
-            else if (pose.heading == 'W')
-            {
-                --pose.x;
-            }
-            else if (pose.heading == 'N')
-            {
-                ++pose.y;
-            }
-            else if (pose.heading == 'S')
-            {
-                --pose.y;
-            }
-        }
-        else if (cmd == 'L')
-        {
-            if (pose.heading == 'E')
-            {
+        if (cmd == 'M') {
+            Move();
+        } else if (cmd == 'L') {
+            if (pose.heading == 'E') {
                 pose.heading = 'N';
-            }
-            else if (pose.heading == 'N')
-            {
+            } else if (pose.heading == 'N') {
                 pose.heading = 'W';
-            }
-            else if (pose.heading == 'W')
-            {
+            } else if (pose.heading == 'W') {
                 pose.heading = 'S';
+            } else if (pose.heading == 'S') {
+                pose.heading = 'E';
             }
-            else if (pose.heading == 'S')
-            {
+        } else if (cmd == 'R') {
+            if (pose.heading == 'E') {
+                pose.heading = 'S';
+            } else if (pose.heading == 'S') {
+                pose.heading = 'W';
+            } else if (pose.heading == 'W') {
+                pose.heading = 'N';
+            } else if (pose.heading == 'N') {
                 pose.heading = 'E';
             }
         }
-        else if (cmd == 'R')
-        {
-            if (pose.heading == 'E')
-            {
-                pose.heading = 'S';
-            }
-            else if (pose.heading == 'S')
-            {
-                pose.heading = 'W';
-            }
-            else if (pose.heading == 'W')
-            {
-                pose.heading = 'N';
-            }
-            else if (pose.heading == 'N')
-            {
-                pose.heading = 'E';
-            }
-        }
+    }
+}
+void ExecutorImpl::Move() noexcept
+{
+    if (pose.heading == 'E') {
+        pose.x += 1;
+    } else if (pose.heading == 'W') {
+        pose.x -= 1;
+    } else if (pose.heading == 'N') {
+        pose.y += 1;
+    } else if (pose.heading == 'S') {
+        pose.y -= 1;
     }
 }
 
