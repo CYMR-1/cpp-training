0c02e64ca03fefae5eb8897c11c2cc148bbf1fcd
CYMR-1 <cymr1234@outlook.com>
2025-12-23 20:04:43 +0800

    ICommand

--- src/ExecutorImpl.hpp
+++ src/ExecutorImpl.hpp
@@ -15,32 +15,38 @@ public:
     Pose Query(void) const noexcept override;
 
 private:
-    void Move(void) noexcept;
-    void TurnLeft(void) noexcept;
-    void TurnRight(void) noexcept;
     Pose pose;
 
 private:
-    class MoveCommand final
+    void Move(void) noexcept;
+    void TurnLeft(void) noexcept;
+    void TurnRight(void) noexcept;
+    class ICommand
     {
     public:
-        void DoOperate(ExecutorImpl& executor) const noexcept
+        virtual ~ICommand() = default;
+        virtual void DoOperate(ExecutorImpl& executor) const noexcept = 0;
+    };
+    class MoveCommand final:public ICommand
+    {
+    public:
+        void DoOperate(ExecutorImpl& executor) const noexcept override
         {
             executor.Move();
         }
     };
-    class TurnLeftCommand final
+    class TurnLeftCommand final: public ICommand
     {
     public:
-        void DoOperate(ExecutorImpl& executor) const noexcept
+        void DoOperate(ExecutorImpl& executor) const noexcept override
         {
             executor.TurnLeft();
         }
     };
-    class TurnRightCommand final
+    class TurnRightCommand final: public ICommand
     {
     public:
-        void DoOperate(ExecutorImpl& executor) const noexcept
+        void DoOperate(ExecutorImpl& executor) const noexcept override
         {
             executor.TurnRight();
         }
