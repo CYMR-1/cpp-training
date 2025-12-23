ed33941f577e0e65d0bf9cd887609cf636116da3
CYMR-1 <cymr1234@outlook.com>
2025-12-23 19:32:39 +0800

    extract TurnLeft()

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -14,15 +14,7 @@ void ExecutorImpl::Execute(const std::string& commands) noexcept
         if (cmd == 'M') {
             Move();
         } else if (cmd == 'L') {
-            if (pose.heading == 'E') {
-                pose.heading = 'N';
-            } else if (pose.heading == 'N') {
-                pose.heading = 'W';
-            } else if (pose.heading == 'W') {
-                pose.heading = 'S';
-            } else if (pose.heading == 'S') {
-                pose.heading = 'E';
-            }
+            TurnLeft();
         } else if (cmd == 'R') {
             if (pose.heading == 'E') {
                 pose.heading = 'S';
@@ -46,6 +38,18 @@ void ExecutorImpl::Move() noexcept
         pose.y += 1;
     } else if (pose.heading == 'S') {
         pose.y -= 1;
+    }
+}
+void ExecutorImpl::TurnLeft() noexcept
+{
+    if (pose.heading == 'E') {
+        pose.heading = 'N';
+    } else if (pose.heading == 'N') {
+        pose.heading = 'W';
+    } else if (pose.heading == 'W') {
+        pose.heading = 'S';
+    } else if (pose.heading == 'S') {
+        pose.heading = 'E';
     }
 }
 
